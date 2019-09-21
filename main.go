8368aad669b477d3995ad0b48ed716503b73276b
refoilmiya <refo-intern@payfazz.com>
2019-09-21 23:58:04 +0700

    initial commit konselink

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"konselink/app"
+	"konselink/controllers"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/users/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/users/login", controllers.Authenticate).Methods("POST")
+
+
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+
+	//router.NotFoundHandler = app.NotFoundHandler
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":" + port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
+}
